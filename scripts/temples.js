79aa0323f0759edd26743afc97c3a3e08722b943
francahc <franca.harris@gmail.com>
2024-10-02 21:59:37 -0600

    comments

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,7 +2,7 @@ document.getElementById('currentyear').textContent = new Date().getFullYear();
 
 document.getElementById('lastModified').textContent = `Last modified: ${document.lastModified}`;
 
-// Function to update footer year and last modified date
+
 function updateFooter() {
     const footer = document.querySelector('footer');
     const year = new Date().getFullYear();
@@ -10,18 +10,16 @@ function updateFooter() {
     footer.innerHTML = `&copy; ${year} | Last Modified: ${lastModified}`;
 }
 
-// Hamburger menu functionalit
 
-// Obtener referencias al botón hamburguesa y al menú de navegación
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.querySelector('nav ul');
 
-// Agregar evento al botón hamburguesa
+
 hamburger.addEventListener('click', () => {
     if (navMenu.style.display === 'flex') {
-        navMenu.style.display = 'none'; // Ocultar el menú
+        navMenu.style.display = 'none'; 
     } else {
-        navMenu.style.display = 'flex'; // Mostrar el menú
+        navMenu.style.display = 'flex'; 
     }
 });
 
